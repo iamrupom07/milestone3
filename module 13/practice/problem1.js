c9060aaa6b6a7b7fe09277c176d887793c58f055
Rupom <saidurrahmanrupom@gmail.com>
2025-08-31 12:43:01 +0600

    first commit

---
+++ module 13/practice/problem1.js
@@ -0,0 +1,7 @@
+var totalTaka = 1000;
+var orange = 300;
+var apple = 250;
+var totalCost = orange + apple;
+console.log(totalTaka)
+console.log(totalCost);
+console.log(totalTaka - totalCost);
---
+++ module 13/practice/problem3.js
@@ -0,0 +1,4 @@
+var input = 119;
+var result = input % 5;
+console.log(result);
+
