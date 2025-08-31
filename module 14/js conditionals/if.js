7a22a8fd95d1a3ed38968757d6c4008fc7a08dc4
Rupom <saidurrahmanrupom@gmail.com>
2025-08-31 22:14:09 +0600

    if-else

---
+++ module 14/js conditionals/if.js
@@ -0,0 +1,5 @@
+var biryaniPrice = 259;
+
+if (biryaniPrice < 300){
+    console.log('I will buy')
+}
---
+++ module 14/js conditionals/stepped.js
@@ -0,0 +1,10 @@
+const price = 3000;
+
+if (price >= 5000){
+    const discount = price * 10 /100;
+    const payAmount = price - discount;
+    console.log(payAmount)
+}
+else{
+    console.log(price)
+}
